ebfeb6cd4d0b5d73cac0474fbb4ba85f0ffed515
Jon Toussaint <jondtoussaint@gmail.com>
2023-04-15 15:16:01 -0400

    Initial Commit

---
+++ AnalogTodoClone/AnalogTodoCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogTodoCloneApp.swift
+//  AnalogTodoClone
+//
+//  Created by Jon Toussaint on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AnalogTodoCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
